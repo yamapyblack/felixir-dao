cd9b68fd3e28d3c818e3ce0ca81569343a4cc21a
yamapyblack <s25050730r@gmail.com>
2022-03-16 19:18:01 +0900

    nuxt3 init

---
+++ packages/frontend/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ethers } from "ethers"
+
+const hoge = (i: number) => {
+  return i
+}
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <main>
+      <div class="">
+        index
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
