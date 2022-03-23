37ec2ccca2f12e63b886d23f24f0c87808c1a406
tabei takaaki <tabeitakaaki@tabeinombp.lan>
2022-03-24 00:34:09 +0900

    エラーが出ているソースをプッシュ

--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
+
+<script setup lang=ts>
 import { ethers } from "ethers"
+
 
 const hoge = (i: number) => {
   return i
 }
+
+
+const accounts  = async () => { 
+    const addrs = await (window as any).ethereum.request({
+        method: "eth_requestAccounts",
+    });
+ console.log(addrs[0])
+};
+// A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+
+// MetaMask requires requesting permission to connect users accounts
+await provider.send("eth_requestAccounts", []);
+
+// The MetaMask plugin also allows signing transactions to
+// send ether and pay to change state within the blockchain.
+// For this, you need the account signer...
+const signer = provider.getSigner()
 </script>
 
 <template>
@@ -16,5 +37,5 @@ const hoge = (i: number) => {
   </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 </style>
