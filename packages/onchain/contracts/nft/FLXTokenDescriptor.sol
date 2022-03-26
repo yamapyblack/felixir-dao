3eb2cea87f489c4cb6453e76cafc8b1a6f523b69
yamapyblack <s25050730r@gmail.com>
2022-03-26 23:52:48 +0900

    junction svg

--- packages/onchain/contracts/nft/FLXTokenDescriptor.sol
+++ packages/onchain/contracts/nft/FLXTokenDescriptor.sol
@@ -5,25 +5,83 @@ pragma solidity ^0.8.9;
 import "../interfaces/ITokenDescriptor.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
-import './FLXSvgBase.sol';
+import { Base64 } from '@openzeppelin/contracts/utils/Base64.sol';
+import { MultiPartRLEToSVG } from '../lib/MultiPartRLEToSVG.sol';
 
 contract FLXTokenDescriptor is ITokenDescriptor, Ownable {
     using Strings for uint256;
 
-    string[6] colorName = ["red","green","yellow","blue","white","black"];
+    struct SvgParam {
+        bytes seed;
+        string[] palette;
+    }
+
+    mapping(uint8 => string[]) public palettes;
+    mapping(uint8 => bytes) public seeds;
 
     string _name = "CryptoFrontier Character #";
     string _description = 'CryptoFrontier is the Full On-chained Game by FrontierDAO.\\n\\nFull On-chained Game is the game where all the data of the game is stored on-chain. Not only the NFTs, but also characters status, skills, battle results and even the battle logic are recorded on-chain.\\nhttps://medium.com/@yamapyblack/full-on-chained-game-by-frontierdao-b8e50549811d';
 
     constructor() {}
 
-    function setName(string memory name) external onlyOwner {
-        _name = name;
+    function setName(string memory name) external onlyOwner {_name = name;}
+    function setDescription(string memory description) external onlyOwner {_description = description;}
+
+    function addSeed(uint8 _seedIdx, bytes calldata _seed) external onlyOwner {
+        seeds[_seedIdx] = _seed;
     }
 
-    function setDescription(string memory description) external onlyOwner {
-        _description = description;
+    /**
+     * @notice Add colors to a color palette.
+     * @dev This function can only be called by the owner.
+     */
+    function addBulkColorsToPalette(uint8 paletteIndex, string[] calldata newColors) external onlyOwner {
+        require(palettes[paletteIndex].length + newColors.length <= 256, 'Palettes can only hold 256 colors');
+        for (uint256 i = 0; i < newColors.length; i++) {
+            _addColorToPalette(paletteIndex, newColors[i]);
+        }
+    }
+
+    /**
+     * @notice Add a single color to a color palette.
+     * @dev This function can only be called by the owner.
+     */
+    function addColorToPalette(uint8 _paletteIndex, string calldata _color) external onlyOwner {
+        require(palettes[_paletteIndex].length <= 255, 'Palettes can only hold 256 colors');
+        _addColorToPalette(_paletteIndex, _color);
+    }
+
+    /**
+     * @notice Add a single color to a color palette.
+     */
+    function _addColorToPalette(uint8 _paletteIndex, string calldata _color) internal {
+        palettes[_paletteIndex].push(_color);
+    }
+
+    function _buildAttributes(
+        string[3] memory trait_types,
+        string[3] memory values
+    ) private pure returns (string memory) {
+        string memory ret = "[";
+
+        for (uint8 i = 0; i < values.length; i++) {
+
+            if(i == 0){ //first
+                ret = string(
+                    abi.encodePacked(
+                        ret,'{"trait_type": "',trait_types[i],'","value": "',values[i],'"}'
+                    )
+                );
+            }else{
+                ret = string(
+                    abi.encodePacked(
+                        ret,',','{"trait_type": "',trait_types[i],'","value": "',values[i],'"}'
+                    )
+                );
+            }
+        }
+
+        return string(abi.encodePacked(ret, "]"));
     }
 
     function _generateName(uint256 tokenId)
@@ -59,62 +117,42 @@ contract FLXTokenDescriptor is ITokenDescriptor, Ownable {
         return _buildAttributes(attributes, values);
     }
 
-    function _buildAttributes(
-        string[3] memory trait_types,
-        string[3] memory values
-    ) private pure returns (string memory) {
-        string memory ret = "[";
-
-        for (uint8 i = 0; i < values.length; i++) {
-
-            if(i == 0){ //first
-                ret = string(
-                    abi.encodePacked(
-                        ret,
-                        '{"trait_type": "',
-                        trait_types[i],
-                        '","value": "',
-                        values[i],
-                        '"}'
-                    )
-                );
-            }else{
-                ret = string(
-                    abi.encodePacked(
-                        ret,
-                        ',',
-                        '{"trait_type": "',
-                        trait_types[i],
-                        '","value": "',
-                        values[i],
-                        '"}'
-                    )
-                );
-            }
-        }
-
-        return string(abi.encodePacked(ret, "]"));
-    }
-
-    function _generateImage(uint256 tokenId)
-        internal
+    function generateImage(uint256 tokenId)
+        public
         view
         returns (string memory)
     {
-        return
-            Base64.encode(
-                bytes(
-                    abi.encodePacked(
-                        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 320 320">',
-                        "<style>",
-                        "</style>",
-                        FLXSvgBase.basePixel1(),
-                        "</svg>"
-                    )
-                )
-            );
-    }
 
+        string memory ret = '<svg width="320" height="320" viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges">';
+
+        ret = string(
+            abi.encodePacked(
+                ret,
+        // TODO yamaura
+                MultiPartRLEToSVG.generateSVGRects(seeds[0], palettes[0])
+            )
+        );
+
+        // TODO yamaura
+        ret = string(
+            abi.encodePacked(
+                ret,
+                '<rect x="150" y="30" width="10" height="10" fill="#f00"/><rect x="160" y="30" width="10" height="10" fill="#0f0"/>',
+                '<rect x="150" y="30" width="10" height="10" fill="#f00"/><rect x="160" y="30" width="10" height="10" fill="#00f"/>'
+            )
+        );
+
+        // prettier-ignore
+        return Base64.encode(bytes(
+            string(
+                abi.encodePacked(
+                    ret,
+                    '</svg>'
+                )
+            )
+        ));
+
+    }
 
     function tokenURI(IERC721 token, uint256 tokenId)
         public
@@ -136,7 +174,7 @@ contract FLXTokenDescriptor is ITokenDescriptor, Ownable {
                                 '", "attributes":',
                                 _generateAttributes(tokenId),
                                 ', "image": "data:image/svg+xml;base64,',
-                                _generateImage(tokenId),
+                                generateImage(tokenId),
                                 '"}'
                             )
                         )
