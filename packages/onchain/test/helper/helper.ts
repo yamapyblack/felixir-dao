249bf9c1b35503578453a2462d6a14c5dd84ca3f
yamapyblack <s25050730r@gmail.com>
2022-04-06 19:21:56 +0900

    executor test

--- packages/onchain/test/helper/helper.ts
+++ packages/onchain/test/helper/helper.ts
@@ -1,5 +1,10 @@
 import { network, ethers } from "hardhat";
 
+export const encodeParameters = (types: string[], values: unknown[]): string => {
+  const abi = new ethers.utils.AbiCoder()
+  return abi.encode(types, values)
+}
+
 export const evmMine = async (num: number): Promise<void> => {
   for(let i = 0; i < num; i++){
     await network.provider.send("evm_mine")
