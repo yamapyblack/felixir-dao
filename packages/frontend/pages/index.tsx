072c775806201ae154eaee6645bbc88dad97c2b7
tabeaki <tabeitakaaki@gmail.com>
2022-04-17 01:29:59 +0900

    mint button hide

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -5,6 +5,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
 declare global {
   interface Window {
     ethereum: any;
@@ -13,9 +16,9 @@ declare global {
 
 const Home: NextPage = () => {
   let mintNumber = 0;
-  const [isThumbnail, setIsThumbnail] = useState(true);
-  const mintFlag = false;
-  const tokenPrice = 0.1;
+  let mintFlag:boolean = false;
+  const tokenPrice = "0.1";
+  
   // add Network
   function addChain() {
     try{
@@ -35,23 +38,7 @@ const Home: NextPage = () => {
     }catch(Exeption){
       console.log("Astar Network already Connected");
     } 
-    //   (window as any).ethereum.request({
-    //     method: 'wallet_addEthereumChain',
-    //     params: [{
-    //       chainId: '0x13881',
-    //       chainName: 'mumbai',
-    //       nativeCurrency: {
-    //           name: 'MATIC',
-    //           symbol: 'MATIC',
-    //           decimals: 18,
-    //       },
-    //       rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
-    //     }],
-    //   })
-    // }catch(Exeption){
-    //   console.log("Astar Network already Connected");
-    // } 
-  };
+  }
   addChain();
 
   // ミントボタン用
@@ -68,21 +55,23 @@ const Home: NextPage = () => {
         ]
       const contractAddress = "0x9d8148d2382eaF2c2E917a34341e7A38Aa7Cb120"
       const contract = new ethers.Contract(contractAddress, abi, signer);
-      contract.buy({value: ethers.utils.parseEther("0.1")});
+      //mintNumber = contract.counter();
+      //mintFlag = contract.
+      contract.buy({value: ethers.utils.parseEther(tokenPrice)});
     };
     return <>
     <div className="flex item-center border-gray-600 h-96">
-      <div className="flex-1 text-right py-2 m-2 pt-12">
+      <div className="flex-1 text-right py-2 m-2 pt-16">
         <Image className="" src="/fel_2.png" alt="chara1"  width={256} height={256} objectFit="contain"/>
       </div>
       <div className="flex-1 px-4 py-2 m-2 pt-16">
         <div className="flex-1 border-double border-4 rounded-md bg-sky-500 px-4 py-2 m-2">
           <h3 className="text-4xl text-white font-semibold ">NFT Initial sale</h3>
-          <h3 className="text-4xl pt-1 text-white font-semibold ">April 18th</h3>
-          <h1 className="text-5xl pt-1 text-white font-semibold "></h1>
+          <h1 className="text-2xl pt-1 text-white font-semibold ">START DATE: April 18th</h1>
+          <h1 className="text-2xl pt-1 text-white font-semibold ">14:00(UTC) | 23:00(JST)</h1>
           <h1 className="text-5xl pt-1 pb-2 text-white font-semibold "> {mintNumber} / 4000</h1>
           {(() => {
-            if (!mintFlag) {
+            if (mintFlag) {
               return <button id="mintButton" className="px-2 py-1 my-1 text-2xl text-white font-semibold rounded-full bg-rose-700" onClick={MetaMuskConnect}>NFT MINT</button>       
             } else if(mintNumber == 4000){
               return <h3 className="text-3xl pt-1 text-white font-semibold ">End of sale</h3>
@@ -92,7 +81,7 @@ const Home: NextPage = () => {
           })()} 
           </div>
       </div>
-      <div className="flex-1 text-left px-4 py-2 m-2 pt-12">
+      <div className="flex-1 text-left px-4 py-2 m-2 pt-16">
         <Image className="" src="/fel_1.png" alt="chara2"  width={256} height={256} objectFit="contain"/>
       </div>
     </div>
