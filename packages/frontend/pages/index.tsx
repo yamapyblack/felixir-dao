667b4daf066f8b4b83b17f40c7ffe33ed0d89275
yamapyblack <s25050730r@gmail.com>
2022-04-17 18:14:31 +0900

    fix

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers"
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -12,10 +12,51 @@ declare global {
   }
 }
 
+const abi = [
+  "function buy() external payable",
+  "function isSaleNow() view returns(bool)",
+  "function counter() view returns(uint)"
+]
+// TODO yamaura
+const contractAddress = "0x40581657AD36237baE43B7B2A67a63dfd31111b5"
+
 const Home: NextPage = () => {
-  let mintNumber = 0;
-  let mintFlag:boolean = false;
+  // let mintNumber = 0;
+  // let mintFlag:boolean = false;
+
+  //TODO yamaura
   const tokenPrice = "0.1";
+
+  const [mintNum, setMintNum] = useState(0);
+  const [saleFlag, setSaleFlag] = useState(false);
+
+  useEffect(() => {
+    const setSaleInfo = async() =>{
+      console.log("setSaleInfo")
+  
+      const provider = new ethers.providers.Web3Provider((window as any).ethereum)  
+      console.log(provider)
+  
+      const accounts =  await provider.send("eth_requestAccounts", []);
+      
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+
+      try{
+        const mintNumber = (await contract.counter()).toString() - 1;
+        const saleFlag = await contract.isSaleNow();
+        console.log("mintNumber", mintNumber);
+        console.log("saleFlag", saleFlag);
+        setMintNum(mintNumber)
+        setSaleFlag(saleFlag)  
+      }catch(e){
+        console.log(e)
+      }
+    }
+
+    setSaleInfo();
+  }, []);
+
 
   // add Network
   function addChain() {
@@ -41,26 +82,21 @@ const Home: NextPage = () => {
 
   // ミントボタン用
   function MintButton() {
+    console.log("MintButton")
+
     const MetaMuskConnect = async () =>{
+      console.log("MetaMuskConnect")
+
       addChain();
       const provider = new ethers.providers.Web3Provider((window as any).ethereum)
   
       const accounts =  await provider.send("eth_requestAccounts", []);
       
       const signer = provider.getSigner()
-      const abi = [
-        "function buy() external payable nonReentrant",
-        "function isSaleNow() returns(bool)",
-        "function counter() returns(bool)"
-        ]
-      const contractAddress = "0x9d8148d2382eaF2c2E917a34341e7A38Aa7Cb120"
       const contract = new ethers.Contract(contractAddress, abi, signer);
-      mintNumber = contract.counter();
-      mintFlag = contract.isSaleNow();
-      console.log(mintNumber);
-      console.log(mintFlag);
       contract.buy({value: ethers.utils.parseEther(tokenPrice)});
     };
+
     return <>
     <div className="flex item-center border-gray-600 h-96">
       <div className="flex-1 text-right py-2 m-2 pt-16">
@@ -68,19 +104,15 @@ const Home: NextPage = () => {
       </div>
       <div className="flex-1 px-4 py-2 m-2 pt-16">
         <div className="flex-1 border-double border-4 rounded-md bg-sky-500 px-4 py-2 m-2">
-          <h3 className="text-4xl text-white font-semibold ">NFT Initial sale</h3>
+          <h3 className="text-4xl text-white font-semibold ">NFT Initial Sale</h3>
           <h1 className="text-2xl pt-1 text-white font-semibold ">START DATE: April 18th</h1>
           <h1 className="text-2xl pt-1 text-white font-semibold ">14:00(UTC) | 23:00(JST)</h1>
-          <h1 className="text-5xl pt-1 pb-2 text-white font-semibold "> {mintNumber} / 4000</h1>
-          {(() => {
-            if (mintFlag) {
-              return <button id="mintButton" className="px-2 py-1 my-1 text-2xl text-white font-semibold rounded-full bg-rose-700" onClick={MetaMuskConnect}>NFT MINT</button>       
-            } else if(mintNumber == 4000){
-              return <h3 className="text-3xl pt-1 text-white font-semibold ">End of sale</h3>
-            } else {
-              return <h3 className="text-3xl pt-1 text-white font-semibold ">Wait till the sale</h3>
-            }
-          })()} 
+          <h1 className="text-5xl pt-1 pb-2 text-white font-semibold "> {mintNum} / 4000</h1>
+
+          { !saleFlag && <h3 className="text-3xl pt-1 text-white font-semibold ">Wait until the sale</h3>}
+          { (saleFlag && mintNum < 4000) && <button id="mintButton" className="px-2 py-1 my-1 text-2xl text-white font-semibold rounded-full bg-rose-700" onClick={MetaMuskConnect}>NFT MINT</button>}
+          { (saleFlag && mintNum >= 4000) && <h3 className="text-3xl pt-1 text-white font-semibold ">End of sale</h3>}
+
           </div>
       </div>
       <div className="flex-1 text-left px-4 py-2 m-2 pt-16">
@@ -107,6 +139,7 @@ const Home: NextPage = () => {
     </div>
     </>
   }
+
   return (
     <div>
       <Header />
