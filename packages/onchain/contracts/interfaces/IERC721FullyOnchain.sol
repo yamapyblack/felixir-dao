a150e694ccc00f5647d757307fcf9771a787dbb3
yamapyblack <s25050730r@gmail.com>
2022-04-27 17:09:57 +0900

    delete onchain

--- packages/onchain/contracts/interfaces/IERC721FullyOnchain.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: CC0
-
-pragma solidity ^0.8.0;
-
-interface IERC721FullyOnchain{
-    function descriptor() view external returns(address);
-    function setDescriptor(address _descriptor) external;
-}
--- packages/onchain/contracts/mock/PaletteStorageMock.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: CC0
-
-pragma solidity 0.8.13;
-
-import "../lib/PaletteStorage.sol";
-
-contract PaletteStorageMock is PaletteStorage {
-}
